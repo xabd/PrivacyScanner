793faa966b3a6945b4c0aa2aa463c5043cc8dd0a
xabd <hichamwalid664@gmail.com>
2025-10-07 15:56:10 +0200

    fix: align Gradle and F-Droid build setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "nodomain.xabd.privacyscanner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.2"
+        versionCode = 7
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
