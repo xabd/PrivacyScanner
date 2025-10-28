aac16f9866859cc91f49e1a9008a7b7b80f6632e
xabd <hichamwalid664@gmail.com>
2025-10-28 10:19:12 +0100

    Bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "nodomain.xabd.privacyscanner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.0"
+        versionCode = 10
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
